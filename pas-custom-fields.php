ed27ec5b7ec64825540a35854e646c9cc1f11a19
Yaa Otchere <yaa@yaaotchere.ca>
2017-11-06 16:27:55 -0500

    adding my base plugins

---
+++ pas-custom-fields.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Pacific Art Stone Custom Fields
+Description: Register custom fields for Pacific Art Stone website.
+Version:     1
+Author:      Yaa Otchere
+Author URI:  http://yaaotchere.ca
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
